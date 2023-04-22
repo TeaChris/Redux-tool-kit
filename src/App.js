bbc6b3affedced336544812e4d71595e30c64bb4
bermuda <teechris110@gmail.com>
2023-04-22 18:44:07 +0100

    modal features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react'
+
 import CartContainer from './components/CartContainer'
 import Navbar from './components/Navbar'
+import { useDispatch, useSelector } from 'react-redux'
+import { calculateTotal } from './features/cart/cartSlice'
+import Modal from './components/Modal'
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(calculateTotal())
+  }, [cartItems])
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
